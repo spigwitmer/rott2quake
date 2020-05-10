35d9878023865dfd8270c678736a05a301290462
camtap <camtap@fastmail.com>
2020-05-10 15:55:04 -0400

    plane decompression

--- rtl.go
+++ rtl.go
@@ -16,7 +16,7 @@ type RTLHeader struct {
     Version uint32
     Used uint32
     CRC [4]byte
-    RLEWTag [4]byte
+    RLEWTag uint32
     MapSpecials uint32
     WallPlaneOffset uint32
     SpritePlaneOffset uint32
@@ -30,6 +30,9 @@ type RTLHeader struct {
 type RTL struct {
     fhnd io.ReadSeeker
     Header RTLHeader
+    WallPlane [128][128]uint16
+    SpritePlane [128][128]uint16
+    InfoPlane [128][128]uint16
 }
 
 func NewRTL(rfile io.ReadSeeker) (*RTL, error) {
@@ -44,7 +47,58 @@ func NewRTL(rfile io.ReadSeeker) (*RTL, error) {
         return nil, fmt.Errorf("not an RTL file")
     }
 
+    if err := r.decompressWallPlane(); err != nil {
+        return nil, err
+    }
+    if err := r.decompressSpritePlane(); err != nil {
+        return nil, err
+    }
+    if err := r.decompressInfoPlane(); err != nil {
+        return nil, err
+    }
+
     return &r, nil
+}
+
+func (r *RTL) decompressPlane(plane *[128][128]uint16, rlewtag uint32) (error) {
+    var curValue uint16
+    
+    for i := 0; i < 128; {
+        if err := binary.Read(r.fhnd, binary.LittleEndian, &curValue); err != nil {
+            return err
+        }
+
+        if curValue != uint16(rlewtag) {
+            plane[i / 128][i % 128] = curValue
+            i += 1
+        } else {
+            var count uint16
+
+            if err := binary.Read(r.fhnd, binary.LittleEndian, &count); err != nil {
+                return err
+            }
+            if err := binary.Read(r.fhnd, binary.LittleEndian, &curValue); err != nil {
+                return err
+            }
+
+            for j := uint16(0); j < count; j += 1 {
+                plane[i / 128][i % 128] = curValue
+                i += 1
+            }
+        }
+    }
+
+    return nil
+}
+
+func (r *RTL) decompressWallPlane() (error) {
+    return r.decompressPlane(&r.WallPlane, r.Header.RLEWTag)
+}
+func (r *RTL) decompressSpritePlane() (error) {
+    return r.decompressPlane(&r.SpritePlane, r.Header.RLEWTag)
+}
+func (r *RTL) decompressInfoPlane() (error) {
+    return r.decompressPlane(&r.InfoPlane, r.Header.RLEWTag)
 }
 
 func (r *RTL) MapName() (string) {
