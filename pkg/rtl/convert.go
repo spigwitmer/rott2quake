031041b96a3999f2708462db9cc9cbbea92294d3
spigwitmer <itgpmc@gmail.com>
2020-11-22 17:23:35 -0800

    Initial commit of elevator support

--- pkg/rtl/convert.go
+++ pkg/rtl/convert.go
@@ -40,6 +40,174 @@ func ClassNameForMaskedWall(w *MaskedWallInfo, position string) string {
 	return "func_detail"
 }
 
+type ElevatorNode struct {
+	Switch WallInfo
+	Floor  WallInfo
+}
+
+// Adds func_button and trigger_teleport entities to link elevators
+func LinkElevators(rtlmap *RTLMapData, textureWad string,
+	floorDepth, gridSizeX, gridSizeY, gridSizeZ, scale float64,
+	dusk bool, qm *quakemap.QuakeMap) {
+	elevators := make(map[uint16][]ElevatorNode)
+
+	elevatorSwitchTile := uint16(0x4c)
+	elevatorDoorTile := uint16(0x66)
+
+	for x := 0; x < 128; x++ {
+		for y := 0; y < 128; y++ {
+			// look for elevator door, calculate floor from where switch
+			// is
+			if rtlmap.WallPlane[x][y] == elevatorDoorTile {
+				if x > 1 && rtlmap.WallPlane[x-2][y] == elevatorSwitchTile {
+					linkCode := rtlmap.SpritePlane[x-1][y]
+					elevators[linkCode] = append(elevators[linkCode], ElevatorNode{
+						Floor:  rtlmap.CookedWallGrid[x-1][y],
+						Switch: rtlmap.CookedWallGrid[x-2][y],
+					})
+				} else if x < 126 && rtlmap.WallPlane[x+2][y] == elevatorSwitchTile {
+					linkCode := rtlmap.SpritePlane[x+1][y]
+					elevators[linkCode] = append(elevators[linkCode], ElevatorNode{
+						Floor:  rtlmap.CookedWallGrid[x+1][y],
+						Switch: rtlmap.CookedWallGrid[x+2][y],
+					})
+				} else if y > 1 && rtlmap.WallPlane[x][y-2] == elevatorSwitchTile {
+					linkCode := rtlmap.SpritePlane[x][y-1]
+					elevators[linkCode] = append(elevators[linkCode], ElevatorNode{
+						Floor:  rtlmap.CookedWallGrid[x][y-1],
+						Switch: rtlmap.CookedWallGrid[x][y-2],
+					})
+				} else if y < 126 && rtlmap.WallPlane[x][y+2] == elevatorSwitchTile {
+					linkCode := rtlmap.SpritePlane[x][y+1]
+					elevators[linkCode] = append(elevators[linkCode], ElevatorNode{
+						Floor:  rtlmap.CookedWallGrid[x][y+1],
+						Switch: rtlmap.CookedWallGrid[x][y+2],
+					})
+				}
+			}
+		}
+	}
+
+	for linkCode, elevatorNodes := range elevators {
+		if len(elevatorNodes) != 2 {
+			log.Printf("Elevator tiles with link code %d != 2???", len(elevatorNodes))
+			continue
+		}
+
+		elev1 := elevatorNodes[0]
+		elev2 := elevatorNodes[1]
+
+		var floor1ButtonX1, floor1ButtonY1, floor1ButtonX2, floor1ButtonY2 float64
+		var floor2ButtonX1, floor2ButtonY1, floor2ButtonX2, floor2ButtonY2 float64
+		var button1Angle, button2Angle string
+
+		if elev1.Switch.X != elev1.Floor.X {
+			// func_button facing east or west
+			floor1ButtonX1 = float64(elev1.Floor.X) * gridSizeX
+			floor1ButtonY1 = float64(elev1.Floor.Y) * gridSizeY
+			floor1ButtonY2 = floor1ButtonY1 + gridSizeY
+			if elev1.Switch.X > elev1.Floor.X {
+				floor1ButtonX1 += gridSizeX
+				button1Angle = "180"
+			} else {
+				button1Angle = "0"
+			}
+			floor1ButtonX2 = floor1ButtonX1 + 1
+		} else {
+			// func_button facing north or south
+			floor1ButtonY1 = float64(elev1.Floor.Y) * gridSizeY
+			floor1ButtonX1 = float64(elev1.Floor.X) * gridSizeX
+			floor1ButtonX2 = floor1ButtonX1 + gridSizeX
+			if elev1.Switch.Y > elev1.Floor.Y {
+				floor1ButtonY1 += gridSizeY
+				button1Angle = "270"
+			} else {
+				button1Angle = "90"
+			}
+			floor1ButtonY2 = floor1ButtonY1 + 1
+		}
+		if elev2.Switch.X != elev2.Floor.X {
+			floor2ButtonX1 = float64(elev2.Floor.X) * gridSizeX
+			floor2ButtonY1 = float64(elev2.Floor.Y) * gridSizeY
+			floor2ButtonY2 = floor1ButtonY2 + gridSizeY
+			if elev2.Switch.X > elev2.Floor.X {
+				floor2ButtonX1 += gridSizeX
+				button2Angle = "180"
+			} else {
+				button2Angle = "0"
+			}
+			floor2ButtonX2 = floor2ButtonX1 + 1
+		} else {
+			floor2ButtonY1 = float64(elev2.Floor.Y) * gridSizeY
+			floor2ButtonX1 = float64(elev2.Floor.X) * gridSizeX
+			floor2ButtonX2 = floor2ButtonX1 + gridSizeX
+			if elev2.Switch.Y > elev2.Floor.Y {
+				floor2ButtonY1 += gridSizeY
+				button2Angle = "270"
+			} else {
+				button2Angle = "90"
+			}
+			floor2ButtonY2 = floor2ButtonY1 + 1
+		}
+
+		// elevator 1
+		floor1Entity := quakemap.NewEntity(0, "func_button", qm)
+		floor1Entity.AdditionalKeys["target"] = fmt.Sprintf("elev_%d_1_trigger", linkCode)
+		floor1Entity.AdditionalKeys["angle"] = button1Angle
+		floor1Entity.AdditionalKeys["lip"] = "1"
+		floor1Brush := quakemap.BasicCuboid(floor1ButtonX1, floor1ButtonY1, floorDepth,
+			floor1ButtonX2, floor1ButtonY2, float64(rtlmap.FloorHeight()+1)*gridSizeZ,
+			"ELEV5", scale, false)
+		floor1Entity.Brushes = append(floor1Entity.Brushes, floor1Brush)
+		qm.Entities = append(qm.Entities, floor1Entity)
+
+		floor1TriggerEntity := quakemap.NewEntity(0, "trigger_teleport", qm)
+		floor1TriggerEntity.AdditionalKeys["targetname"] = fmt.Sprintf("elev_%d_1_trigger", linkCode)
+		floor1TriggerEntity.AdditionalKeys["target"] = fmt.Sprintf("elev_%d_1", linkCode)
+		floor1TriggerEntityBrush := quakemap.BasicCuboid(float64(elev1.Floor.X)*gridSizeX, float64(elev1.Floor.Y)*gridSizeY, floorDepth,
+			float64(elev1.Floor.X+1)*gridSizeX, float64(elev1.Floor.Y+1)*gridSizeY, floorDepth+gridSizeZ,
+			"__TB_empty", -1.0, false)
+		floor1TriggerEntity.Brushes = append(floor1TriggerEntity.Brushes, floor1TriggerEntityBrush)
+		qm.Entities = append(qm.Entities, floor1TriggerEntity)
+
+		floor1DestEntity := quakemap.NewEntity(0, "info_teleport_destination", qm)
+		floor1DestEntity.OriginX = float64(elev2.Floor.X)*gridSizeX + (gridSizeX / 2)
+		floor1DestEntity.OriginY = float64(elev2.Floor.Y)*gridSizeY + (gridSizeY / 2)
+		floor1DestEntity.OriginZ = floorDepth + (gridSizeZ / 2)
+		floor1DestEntity.AdditionalKeys["targetname"] = fmt.Sprintf("elev_%d_1", linkCode)
+		floor1DestEntity.AdditionalKeys["angle"] = button2Angle
+		qm.Entities = append(qm.Entities, floor1DestEntity)
+
+		// elevator 2
+		floor2Entity := quakemap.NewEntity(0, "func_button", qm)
+		floor2Entity.AdditionalKeys["target"] = fmt.Sprintf("elev_%d_2", linkCode)
+		floor2Entity.AdditionalKeys["angle"] = button2Angle
+		floor2Entity.AdditionalKeys["lip"] = "1"
+		floor2Brush := quakemap.BasicCuboid(floor2ButtonX1, floor2ButtonY1, floorDepth,
+			floor2ButtonX2, floor2ButtonY2, float64(rtlmap.FloorHeight()+1)*gridSizeZ,
+			"ELEV5", scale, false)
+		floor2Entity.Brushes = append(floor2Entity.Brushes, floor2Brush)
+		qm.Entities = append(qm.Entities, floor2Entity)
+
+		floor2TriggerEntity := quakemap.NewEntity(0, "trigger_teleport", qm)
+		floor2TriggerEntity.AdditionalKeys["targetname"] = fmt.Sprintf("elev_%d_2_trigger", linkCode)
+		floor2TriggerEntity.AdditionalKeys["target"] = fmt.Sprintf("elev_%d_2", linkCode)
+		floor2TriggerEntityBrush := quakemap.BasicCuboid(float64(elev2.Floor.X)*gridSizeX, float64(elev2.Floor.Y)*gridSizeY, floorDepth,
+			float64(elev2.Floor.X+1)*gridSizeX, float64(elev2.Floor.Y+1)*gridSizeY, floorDepth+gridSizeZ,
+			"__TB_empty", -1.0, false)
+		floor2TriggerEntity.Brushes = append(floor2TriggerEntity.Brushes, floor2TriggerEntityBrush)
+		qm.Entities = append(qm.Entities, floor2TriggerEntity)
+
+		floor2DestEntity := quakemap.NewEntity(0, "info_teleport_destination", qm)
+		floor2DestEntity.OriginX = float64(elev1.Floor.X)*gridSizeX + (gridSizeX / 2)
+		floor2DestEntity.OriginY = float64(elev1.Floor.Y)*gridSizeY + (gridSizeY / 2)
+		floor2DestEntity.OriginZ = floorDepth + (gridSizeZ / 2)
+		floor2DestEntity.AdditionalKeys["targetname"] = fmt.Sprintf("elev_%d_2", linkCode)
+		floor2DestEntity.AdditionalKeys["angle"] = button1Angle
+		qm.Entities = append(qm.Entities, floor2DestEntity)
+	}
+}
+
 func ConvertRTLMapToQuakeMapFile(rtlmap *RTLMapData, textureWad string, scale float64, dusk bool) *quakemap.QuakeMap {
 
 	// worldspawn:
@@ -47,9 +215,9 @@ func ConvertRTLMapToQuakeMapFile(rtlmap *RTLMapData, textureWad string, scale fl
 	var gridSizeX float64 = 64.0 * scale
 	var gridSizeY float64 = 64.0 * scale
 	var gridSizeZ float64 = 64.0 * scale
-	var floorLength float64 = gridSizeX * 128
-	var floorWidth float64 = gridSizeY * 128
-	var floorDepth float64 = 64 * scale
+	var floorLength float64 = gridSizeX * 128.0
+	var floorWidth float64 = gridSizeY * 128.0
+	var floorDepth float64 = 64.0 * scale
 
 	var playerStartX float64 = float64(rtlmap.SpawnX)*gridSizeX + (gridSizeX / 2)
 	var playerStartY float64 = float64(rtlmap.SpawnY)*gridSizeY + (gridSizeY / 2)
@@ -100,8 +268,8 @@ func ConvertRTLMapToQuakeMapFile(rtlmap *RTLMapData, textureWad string, scale fl
 				var x1, y1, x2, y2 float64
 				infoVal := rtlmap.InfoPlane[i][j]
 				spriteVal := rtlmap.SpritePlane[i][j]
-				wallDirection, _, _ := rtlmap.ThinWallDirection(i, j)
 				if infoVal == 1 || (infoVal >= 4 && infoVal <= 9) {
+					wallDirection, _, _ := rtlmap.ThinWallDirection(i, j)
 					if wallDirection == WALLDIR_NorthSouth {
 						x1 = float64(i)*gridSizeX + (gridSizeX / 2) - 2
 						x2 = float64(i)*gridSizeX + (gridSizeX / 2) + 2
@@ -170,6 +338,10 @@ func ConvertRTLMapToQuakeMapFile(rtlmap *RTLMapData, textureWad string, scale fl
 						qm.WorldSpawn.Brushes = append(qm.WorldSpawn.Brushes, wallColumn)
 					}
 				} else {
+					if wallInfo.Tile == 0x4c {
+						// do not render elevator switches
+						continue
+					}
 					// plain ol' column
 					wallColumn := quakemap.BasicCuboid(
 						float64(i)*gridSizeX,   // x1
@@ -422,12 +594,30 @@ func ConvertRTLMapToQuakeMapFile(rtlmap *RTLMapData, textureWad string, scale fl
 		}
 
 		doorEntity.AdditionalKeys["_doornum"] = fmt.Sprintf("%d", doornum)
-		// move upward when open
-		doorEntity.AdditionalKeys["angle"] = "-1"
+		// move upward when open, move sideways if elevator door
+		if door.Tiles[0].Tile == 0x66 {
+			elevTileX, elevTileY := door.Tiles[0].X, door.Tiles[0].Y
+			if elevTileX < 126 && rtlmap.WallPlane[elevTileX+2][elevTileY] == 0x4c {
+				doorEntity.AdditionalKeys["angle"] = "90"
+			} else if elevTileX > 1 && rtlmap.WallPlane[elevTileX-2][elevTileY] == 0x4c {
+				doorEntity.AdditionalKeys["angle"] = "270"
+			} else if elevTileY < 126 && rtlmap.WallPlane[elevTileX][elevTileY+2] == 0x4c {
+				doorEntity.AdditionalKeys["angle"] = "0"
+			} else if elevTileY > 1 && rtlmap.WallPlane[elevTileX][elevTileY-2] == 0x4c {
+				doorEntity.AdditionalKeys["angle"] = "180"
+			} else {
+				log.Printf("Unknown angle for elevator tile at (%d,%d)", elevTileX, elevTileY)
+				doorEntity.AdditionalKeys["angle"] = "-1"
+			}
+		} else {
+			doorEntity.AdditionalKeys["angle"] = "-1"
+		}
 		doorEntity.AdditionalKeys["speed"] = "290"
 		// TODO: keys
 		qm.Entities = append(qm.Entities, doorEntity)
 	}
+
+	LinkElevators(rtlmap, textureWad, floorDepth, gridSizeX, gridSizeY, gridSizeZ, scale, dusk, qm)
 
 	// 2. TODO: clip brushes around floor extending height
 	return qm
