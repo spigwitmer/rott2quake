e0c1b279288b0e4b93aa982e5f99bada7db02242
spigwitmer <itgpmc@gmail.com>
2020-12-18 19:30:15 -0500

    distinguish certain masked walls as switches, fix typo in above texture name

--- pkg/rtl/maskedwall.go
+++ pkg/rtl/maskedwall.go
@@ -7,6 +7,8 @@ type MaskedWallInfo struct {
 	Flags uint16
 	// lump names for each wall component
 	Side, Middle, Above, Bottom string
+	// is it a switch?
+	IsSwitch bool
 }
 
 const (
@@ -54,28 +56,29 @@ const (
 // TODO: this only accounts for the registered version, shareware
 // differs from this
 var MaskedWalls = map[uint16]MaskedWallInfo{
-	MW_HiSwitchOff:         MaskedWallInfo{MWF_Blocking, "", "HSWITCH2", "HSWITCH3", "HSWITCH1"},
-	MW_MultiGlass1:         MaskedWallInfo{MWF_Multi | MWF_Blocking | MWF_BlockingChanges | MWF_Shootable, "SIDE21", "ABOVEM5A", "ABOVEM5", "MULTI1A"},
-	MW_MultiGlass2:         MaskedWallInfo{MWF_Multi | MWF_Blocking | MWF_BlockingChanges | MWF_Shootable, "SIDE21", "ABOVEM5B", "ABOVEM5", "MULTI2A"},
-	MW_MultiGlass3:         MaskedWallInfo{MWF_Multi | MWF_Blocking | MWF_BlockingChanges | MWF_Shootable, "SIDE21", "ABOVEM5C", "ABOVEM5", "MULTI3A"},
-	MW_ShotOutGlass1:       MaskedWallInfo{MWF_BottomPassable, "SIDE21", "ABOVEM5A", "ABOVEM5", "MULTI1"},
-	MW_ShotOutGlass2:       MaskedWallInfo{MWF_BottomPassable, "SIDE21", "ABOVEM5B", "ABOVEM5", "MULTI2"},
-	MW_ShotOutGlass3:       MaskedWallInfo{MWF_BottomPassable, "SIDE21", "ABOVEM5C", "ABOVEM5", "MULTI3"},
-	MW_Normal1:             MaskedWallInfo{MWF_Blocking, "SIDE21", "ABOVEM4A", "ABOVEM4", "MASKED1"},
-	MW_Normal1Shootable:    MaskedWallInfo{MWF_Blocking | MWF_Shootable, "SIDE21", "ABOVEM4A", "ABOVEM4", "MASKED1A"},
-	MW_Normal2:             MaskedWallInfo{MWF_Blocking, "SIDE21", "ABOVEM4A", "ABOVEM4", "MASKED2"},
-	MW_Normal2Shootable:    MaskedWallInfo{MWF_Blocking | MWF_Shootable, "SIDE21", "ABOVEM4A", "ABOVEM4", "MASKED2A"},
-	MW_Normal3:             MaskedWallInfo{MWF_Blocking, "SIDE21", "ABOVEM4A", "ABOVEM4", "MASKED3"},
-	MW_Normal3Shootable:    MaskedWallInfo{MWF_Blocking | MWF_Shootable, "SIDE21", "ABOVEM4A", "ABOVEM4", "MASKED3A"},
-	MW_SinglePaneShootable: MaskedWallInfo{MWF_Shootable | MWF_BlockingChanges | MWF_Blocking, "SIDE21", "ABOVEM4A", "ABOVEM4", "MASKED4A"},
-	MW_SinglePane:          MaskedWallInfo{MWF_BottomPassable, "SIDE21", "ABOVEM4A", "ABOVEM4", "MASKED4"},
-	MW_DogWall:             MaskedWallInfo{MWF_NonDogBlocking | MWF_WeaponBlocking, "SIDE21", "ABOVEM4A", "ABOVEM4", "DOGMASK"},
-	MW_PeepHole:            MaskedWallInfo{MWF_WeaponBlocking | MWF_Blocking, "SIDE21", "ABOVEM4A", "ABOVEM4", "PEEPMASK"},
-	MW_ExitArch:            MaskedWallInfo{MWF_BottomPassable, "SIDE21", "ABOVEM4A", "ABOVEM4", "EXITARCH"},
-	MW_SecretExitArch:      MaskedWallInfo{MWF_BottomPassable, "SIDE21", "ABOVEM4A", "ABOVEM4", "EXITARCA"},
-	MW_EntryGate:           MaskedWallInfo{MWF_Blocking, "SIDE21", "ABOVEM4A", "ABOVEM4", "ENTRARCH"},
-	MW_HiSwitchOn:          MaskedWallInfo{MWF_Blocking | MWF_SwitchOn, "", "HSWITCH2", "HSWITCH4", "HSWITCH1"},
-	MW_Railing:             MaskedWallInfo{MWF_AbovePassable | MWF_MiddlePassable, "", "", "", "RAILING"},
+	MW_HiSwitchOff:         MaskedWallInfo{MWF_Blocking, "", "HSWITCH2", "HSWITCH3", "HSWITCH1", true},
+	MW_MultiGlass1:         MaskedWallInfo{MWF_Multi | MWF_Blocking | MWF_BlockingChanges | MWF_Shootable, "SIDE21", "ABOVEM5A", "ABOVEM5", "MULTI1A", false},
+	MW_MultiGlass2:         MaskedWallInfo{MWF_Multi | MWF_Blocking | MWF_BlockingChanges | MWF_Shootable, "SIDE21", "ABOVEM5B", "ABOVEM5", "MULTI2A", false},
+	MW_MultiGlass3:         MaskedWallInfo{MWF_Multi | MWF_Blocking | MWF_BlockingChanges | MWF_Shootable, "SIDE21", "ABOVEM5C", "ABOVEM5", "MULTI3A", false},
+	MW_ShotOutGlass1:       MaskedWallInfo{MWF_BottomPassable, "SIDE21", "ABOVEM5A", "ABOVEM5", "MULTI1", false},
+	MW_ShotOutGlass2:       MaskedWallInfo{MWF_BottomPassable, "SIDE21", "ABOVEM5B", "ABOVEM5", "MULTI2", false},
+	MW_ShotOutGlass3:       MaskedWallInfo{MWF_BottomPassable, "SIDE21", "ABOVEM5C", "ABOVEM5", "MULTI3", false},
+	MW_Normal1:             MaskedWallInfo{MWF_Blocking, "SIDE21", "ABOVEM4A", "ABOVEM4", "MASKED1", false},
+	MW_Normal1Shootable:    MaskedWallInfo{MWF_Blocking | MWF_Shootable, "SIDE21", "ABOVEM4A", "ABOVEM4", "MASKED1A", false},
+	MW_Normal2:             MaskedWallInfo{MWF_Blocking, "SIDE21", "ABOVEM4A", "ABOVEM4", "MASKED2", false},
+	MW_Normal2Shootable:    MaskedWallInfo{MWF_Blocking | MWF_Shootable, "SIDE21", "ABOVEM4A", "ABOVEM4", "MASKED2A", false},
+	MW_Normal3:             MaskedWallInfo{MWF_Blocking, "SIDE21", "ABOVEM4A", "ABOVEM4", "MASKED3", false},
+	MW_Normal3Shootable:    MaskedWallInfo{MWF_Blocking | MWF_Shootable, "SIDE21", "ABOVEM4A", "ABOVEM4", "MASKED3A", false},
+	MW_SinglePaneShootable: MaskedWallInfo{MWF_Shootable | MWF_BlockingChanges | MWF_Blocking, "SIDE21", "ABOVEM4A", "ABOVEM4", "MASKED4A", false},
+	MW_SinglePane:          MaskedWallInfo{MWF_BottomPassable, "SIDE21", "ABOVEM4A", "ABOVEM4", "MASKED4", false},
+	MW_DogWall:             MaskedWallInfo{MWF_NonDogBlocking | MWF_WeaponBlocking, "SIDE21", "ABOVEM4A", "ABOVEM4", "DOGMASK", false},
+	MW_PeepHole:            MaskedWallInfo{MWF_WeaponBlocking | MWF_Blocking, "SIDE21", "ABOVEM4A", "ABOVEM4", "PEEPMASK", false},
+	MW_ExitArch:            MaskedWallInfo{MWF_BottomPassable, "SIDE21", "ABOVEM4A", "ABOVEM4", "EXITARCH", false},
+	MW_SecretExitArch:      MaskedWallInfo{MWF_BottomPassable, "SIDE21", "ABOVEM4A", "ABOVEM4", "EXITARCA", false},
+	MW_EntryGate:           MaskedWallInfo{MWF_Blocking, "SIDE21", "ABOVEM4A", "ABOVEM4", "ENTRARCH", false},
+	// the typo is intentional, the same typo is in DARKWAR.WAD
+	MW_HiSwitchOn: MaskedWallInfo{MWF_Blocking | MWF_SwitchOn, "", "HSWITCH2", "HSWTICH4", "HSWITCH1", true},
+	MW_Railing:    MaskedWallInfo{MWF_AbovePassable | MWF_MiddlePassable, "", "", "", "RAILING", false},
 }
 
 var HMSK_Lumps = []string{
@@ -99,11 +102,11 @@ var HMSK_Lumps = []string{
 
 // rt_ted.c:2818
 var Platforms = map[int]MaskedWallInfo{
-	4: MaskedWallInfo{MWF_BottomPassable | MWF_MiddlePassable, "", "", HMSK_Lumps[10], ""},
-	5: MaskedWallInfo{MWF_AbovePassable | MWF_MiddlePassable, "", "", "", HMSK_Lumps[8]},
-	6: MaskedWallInfo{MWF_MiddlePassable, "", "", HMSK_Lumps[10], HMSK_Lumps[8]},
-	7: MaskedWallInfo{MWF_BottomPassable, "", HMSK_Lumps[7], HMSK_Lumps[7], HMSK_Lumps[12]},
-	8: MaskedWallInfo{MWF_BottomPassable | MWF_AbovePassable, "", HMSK_Lumps[7], HMSK_Lumps[5], HMSK_Lumps[12]},
-	9: MaskedWallInfo{MWF_AbovePassable, "", HMSK_Lumps[7], HMSK_Lumps[5], HMSK_Lumps[4]},
-	1: MaskedWallInfo{MWF_AbovePassable, "", HMSK_Lumps[7], HMSK_Lumps[5], HMSK_Lumps[4]},
+	4: MaskedWallInfo{MWF_BottomPassable | MWF_MiddlePassable, "", "", HMSK_Lumps[10], "", false},
+	5: MaskedWallInfo{MWF_AbovePassable | MWF_MiddlePassable, "", "", "", HMSK_Lumps[8], false},
+	6: MaskedWallInfo{MWF_MiddlePassable, "", "", HMSK_Lumps[10], HMSK_Lumps[8], false},
+	7: MaskedWallInfo{MWF_BottomPassable, "", HMSK_Lumps[7], HMSK_Lumps[7], HMSK_Lumps[12], false},
+	8: MaskedWallInfo{MWF_BottomPassable | MWF_AbovePassable, "", HMSK_Lumps[7], HMSK_Lumps[5], HMSK_Lumps[12], false},
+	9: MaskedWallInfo{MWF_AbovePassable, "", HMSK_Lumps[7], HMSK_Lumps[5], HMSK_Lumps[4], false},
+	1: MaskedWallInfo{MWF_AbovePassable, "", HMSK_Lumps[7], HMSK_Lumps[5], HMSK_Lumps[4], false},
 }
